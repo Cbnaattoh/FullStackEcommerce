2e6f023ec08ce51415da1c4ea7295c83e3c3969d
unknown <chrisbrandtattoh4@gmail.com>
2024-12-04 11:47:53 +0000

    Drizzle setup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, {json, urlencoded} from 'express';
 import productRoutes from './routes/products/index';
 const port = 3000;
 const app = express()
 app.get('/', (req, res) => {
     res.send('Hello People!')});
 //products endpoints
+app.use(urlencoded({extended: false}));
+app.use(json());
 
 app.use('/products', productRoutes);
 app.listen(port, () => {
