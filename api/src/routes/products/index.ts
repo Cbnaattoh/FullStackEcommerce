5258f18c58675a877f07eed4e65450faabb6329b
unknown <chrisbrandtattoh4@gmail.com>
2024-12-24 16:19:43 +0000

    Authentication and Authorization

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 import { listProducts,getProductById,deleteProduct,updateProduct, createProduct} from "./productsController";
 import { validateData } from "../../middlewares/validationMiddleware";
 import { createProductSchema, updateProductSchema} from "../../db/productsSchema";
+import { verifySeller, verifyToken } from "../../middlewares/authMiddleware";
 
 
 
 const router = Router();
 router.get('/', listProducts);
 router.get('/:id',getProductById);
-router.post('/', validateData(createProductSchema), createProduct);
-router.delete('/:id', deleteProduct);
-router.put('/:id', validateData(updateProductSchema), updateProduct);
+router.post('/', 
+    verifyToken,
+     verifySeller,
+     validateData(createProductSchema), createProduct);
+router.delete('/:id', verifyToken,
+    verifySeller, deleteProduct);
+router.put('/:id',  verifyToken,
+    verifySeller,validateData(updateProductSchema), updateProduct);
 export default router;
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -17,7 +17,7 @@ export async function listProducts (req: Request, res: Response){
 export async function createProduct (req: Request, res: Response){
     console.log(req.body)
   try{ 
-
+     console.log(req.userId);
      const [product]= await db.insert(productsTable)
      .values(req.cleanBody)
      .returning();
